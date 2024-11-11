8d5653c01114fde699b6e4c7f2c3a829f2b47984
Clara Espinosa Pantoja <claritapantoja36@gmail.com>
2024-11-11 15:19:04 +0000

    ejercicios listos

--- .learn/resets/19-Random_colors_loop/app.js
+++ .learn/resets/19-Random_colors_loop/app.js
@@ -18,11 +18,7 @@ function getColor(colorNumber = 0) {
 function getAllStudentColors() {
 
 	//your loop here
-	// let exampleColor = getColor(1);
-	for (let i = 0; i < 10; i++) {
-		let exampleColor = getColor(Math.floor(Math.random() * 4) + 1)
-		console.log(exampleColor);
-	}
+	let exampleColor = getColor(1);
 }
 
 //call the function below with the number of students in the class and print on the console
